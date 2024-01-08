02f449dc6be7cc730be7650ab4b5f69e54db5866
neshamon <jmmatthews@proton.me>
2024-01-07 18:09:37 -0600

    Annotate code

--- continuations.lisp
+++ continuations.lisp
@@ -1,4 +1,20 @@
 ;;; Continuations
+;; Continuations are a programming concept originating from
+;; Scheme, and they are essentially programs frozen in action.
+;; When a frozen object is evaluated the stored
+;; computation is restarted where it left off.
+;; Continuations manage this by using its own copy of the frozen
+;; stack and by ignoring the current stack.
+;; How continuations achieve this is by utilizing a function and
+;; a pointer to the whole stack pending at the moment of creation.
+
+;; Continuations can be useful in solving specific types of problems whether it may be
+;; representing suspended processes in multiprocessing or representing nodes in a search
+;; tree.
+
+;; To depict functions that are continuations we will prepend a function name with the
+;; "=" symbol.
+
 
 (setq *cont* #'identity)
 
