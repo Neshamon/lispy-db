86ebfa1a371604a1be4232932828cdf82e7851af
neshamon <jmmatthews@proton.me>
2024-01-19 01:25:26 -0600

    Add augmented transition networks

--- nondeterministic-algorithms.lisp
+++ nondeterministic-algorithms.lisp
@@ -1,3 +1,11 @@
+;;; Needed Macros
+
+(defmacro with-gensyms (syms &body body)
+  `(let ,(mapcar #'(lambda (s)
+                     `(,s (gensym)))
+          syms)
+     ,@body))
+
 ;;; Continuation Passing Functions / Macros
 
 (setq *cont* #'identity)
@@ -93,6 +101,8 @@ down specific paths, thus enabling it to become deterministic.
       failsym))
 
 (defmacro choose (&rest choices)
+  "The choose macro works by simulating correct guesses
+   on given choices. "
   (if choices
       `(progn
          ,@(mapcar #'(lambda (c)
@@ -130,3 +140,138 @@ down specific paths, thus enabling it to become deterministic.
         (fail))))
 
 (parlor-trick 8) ; => (THE SUM OF 3 5)
+
+;;; Augmented Transition Networks
+
+#| Augmented Transition Networks (ATNs) are a form of parser discovered by
+Bill Woods in the 1970s. They are an early & much more limited form of
+Natural Language Processing algorithms for English.
+
+A transition network is a set of nodes joined together by directed arcs,
+which is essentially a flow chart. Conditions are connected to each arc
+which must be met before the arc can be followed.
+
+Terminal nodes of a transition network will utilized the accumulated
+information to build list structures.
+|#
+
+(defmacro defnode (name &rest arcs)
+  `(=defun ,name (pos regs) (choose ,@arcs)))
+
+;; pos represents the input of the current pointer (an integer)
+;; regs represents the current set of registers (a list of assoc-lists)
+
+(defun compile-cmds (cmds)
+  (if (null cmds)
+      'regs
+      `(,@(car cmds) ,(compile-cmds (cdr cmds)))))
+
+(defmacro jump (next &rest cmds)
+  `(,next pos ,(compile-cmds cmds)))
+
+(defun types (word)
+  "A primitive dictionary that associates
+   each word with a list of one or more
+   simple grammatical roles."
+  (case word
+    ((do does did) '(aux v))
+    ((time times) '(n v))
+    ((fly flies)  '(n v))
+    ((like) '(v prep))
+    ((liked likes) '(v))
+    ((a an the) '(det))
+    ((arrow arrows) '(n))
+    ((i you he she him her it) '(pron))))
+
+(defmacro down (sub next &rest cmds)
+  `(if (= (length *sent*) pos)
+       (fail)
+       (let ((* (nth pos *sent*)))
+         (if (member ',cat (types *))
+             (,next (1+ pos) ,(compile-cmds cmds))
+             (fail)))))
+
+(defmacro up (expr)
+  `(let ((* (nth pos *sent*)))
+     (=values ,expr pos (cdr regs))))
+
+(defmacro getr (key &optional (regs 'regs))
+  "Reads registers"
+  `(let ((result (cdr (assoc ',key (car ,regs)))))
+     (if (cdr result)
+         result
+         (car result))))
+
+(defmacro set-register (key val regs)
+  `(cons (cons (cons ,key ,val) (car ,regs))
+         (cdr ,regs)))
+
+(defmacro setr (key val regs)
+  "Sets registers"
+  `(set-register ',key (list ,val) ,regs))
+
+(defmacro pushr (key val regs)
+  "Pushes to registers"
+  `(set-register ',key
+                 (cons ,val (cdr (assoc ',key (car ,regs))))
+                 ,regs))
+
+(defmacro with-parses (node sent &body body)
+  "Enables us to invoke an ATN"
+  (with-gensyms (pos regs)
+    `(progn
+       (setq *sent* ,sent)
+       (setq *paths* nil)
+       (=bind (parse ,pos ,regs) (,node 0 '(nil))
+         (if (= ,pos (length *sent*))
+             (progn ,@body
+                    (fail))
+             (fail))))))
+
+(defnode mods
+    (cat n mods/n
+         (setr mods *)))
+
+(defnode mods/n
+    (cat n mods/n
+         (pushr mods *))
+  (up `(n-group ,(getr mods))))
+
+(defnode np
+    (cat det np/det
+         (setr det *))
+  (jump np/det
+        (setr det nil))
+  (cat pron pron
+       (setr n *)))
+
+(defnode pron
+    (up `(np (pronoun ,(getr n)))))
+
+(defnode np/det
+    (down mods np/mods
+          (setr mods *))
+  (jump np/mods
+        (setr mods nil)))
+
+(defnode np/mods
+    (cat n np/n
+         (setr n *)))
+
+(defnode np/n
+    (up `(np (det ,(getr det))
+             (modifiers ,(getr mods))
+             (noun ,(getr n))))
+  (down pp np/pp
+        (setr pp *)))
+
+(defnode np/pp
+    (up `(np (det ,(getr det))
+             (modifiers ,(getr mods))
+             (noun ,(getr n))
+             ,(getr pp))))
+
+#| The difference between Paul Graham's Common Lisp implementation
+& SBCL seem to be too great for me to figure out how this code
+should be run. As of now, the ATN does not work. Will debug more
+as I become more knowledgable about Common Lisp. |#
